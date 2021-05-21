f7ac2b1f7080b80a201079a79043770d6a97548a
DevBasito <58977146+DevBasito@users.noreply.github.com>
2021-05-21 18:54:13 +0100

    Update wac.js

--- wac.js
+++ wac.js
@@ -45,7 +45,6 @@ playbtn.addEventListener('click', function () {
         currentTime--;
         document.getElementById('timer').innerHTML= currentTime;
         if (currentTime == 0) {
-            clearInterval(countDownTimer);
             gameStop();
         }
     }
@@ -56,7 +55,7 @@ playbtn.addEventListener('click', function () {
 function gameStop( ) {
     let buzzer= document.getElementById('buzzer');
     buzzer.play();
-
+    clearInterval(countDownTimer);
     clearInterval(game);
     let gameArea= document.querySelector('#gameArea');
     gameArea.style.display= 'none';
